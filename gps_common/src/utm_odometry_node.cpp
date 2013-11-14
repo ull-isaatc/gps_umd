ea49e4fb14c17ccd48cd59f2bc33a0e1ddbff0b5
verdino <verdino@verdino.(none)>
2013-11-14 11:54:12 +0000

    Add fixes for configuring gps serial port, and fix report course over ground.

--- gps_common/src/utm_odometry_node.cpp
+++ gps_common/src/utm_odometry_node.cpp
@@ -7,6 +7,7 @@
 #include <message_filters/time_synchronizer.h>
 #include <sensor_msgs/NavSatStatus.h>
 #include <sensor_msgs/NavSatFix.h>
+#include <gps_common/GPSFix.h>
 #include <gps_common/conversions.h>
 #include <nav_msgs/Odometry.h>
 #include <visualization_msgs/Marker.h>
@@ -20,7 +21,7 @@ double rot_cov;
 double rotacion_incremental, tmp_rot, tmp_rot_mod;
 tf::Quaternion tmp_quat;
 
-void callback(const sensor_msgs::NavSatFixConstPtr& fix) {
+void callback(const gps_common::GPSFixConstPtr& fix) {
   if (fix->status.status == sensor_msgs::NavSatStatus::STATUS_NO_FIX) {
     ROS_WARN("No fix.");
     return;
@@ -49,20 +50,23 @@ void callback(const sensor_msgs::NavSatFixConstPtr& fix) {
 
     odom.pose.pose.position.x = northing;
     odom.pose.pose.position.y = -easting;
-    odom.pose.pose.position.z = fix->altitude;
-    
+//    odom.pose.pose.position.z = fix->altitude;
+    odom.pose.pose.position.z = 0;
+
     tmp_rot = tmp_rot + 0.1;
     tmp_rot_mod = fmod(tmp_rot, 3.1415);
 
-    tmp_quat = tf::createQuaternionFromRPY(0, 0, 0);
+    //tmp_quat = tf::createQuaternionFromRPY(0, 0, 0);
+    tmp_quat = tf::createQuaternionFromRPY(0, 0, -fix->track * RADIANS_PER_DEGREE);
     tf::quaternionTFToMsg(tmp_quat, odom.pose.pose.orientation);
-    
+
+
 //    odom.pose.pose.orientation.x = tmp_quat.getX;
 //    odom.pose.pose.orientation.y = tmp_quat.getY;
 //    odom.pose.pose.orientation.z = tmp_quat.getZ;
 //    odom.pose.pose.orientation.w = tmp_quat.getW;
     //ROS_INFO("tmp_quat =  %f, %f, %f, %f\n", tmp_quat);
-    
+
     // Use ENU covariance to build XYZRPY covariance
     boost::array<double, 36> covariance = {{
       fix->position_covariance[0],
@@ -83,11 +87,12 @@ void callback(const sensor_msgs::NavSatFixConstPtr& fix) {
     }};
 
     odom.pose.covariance = covariance;
-    
-    ROS_DEBUG("UTM publishing at time %f", ros::Time::now().toSec());
-    
+
+//    ROS_DEBUG("UTM publishing at time %f", ros::Time::now().toSec());
+    ROS_DEBUG("GPS UTM odometry publishing with std. deviation in m. (x: %f y: %f, z: %f)", fix->position_covariance[0], fix->position_covariance[4], fix->position_covariance[8]);
+
     odom_pub.publish(odom);
-    
+
     visualization_msgs::Marker marker;
     marker.header.frame_id = odom.header.frame_id;
     marker.header.stamp = fix->header.stamp;
@@ -114,7 +119,7 @@ void callback(const sensor_msgs::NavSatFixConstPtr& fix) {
     marker.scale.y = fix->position_covariance[4];
     //marker.scale.z = 0.01;
     marker.scale.z = fix->position_covariance[8];
-    
+
 
     // Set the color -- be sure to set alpha to something non-zero!
     marker.color.r = 0.0f;
@@ -139,13 +144,13 @@ int main (int argc, char **argv) {
   priv_node.param<double>("rot_covariance", rot_cov, 99999.0);
   priv_node.param<double>("rotacion_incremental", rotacion_incremental, 0);
   tmp_rot = rotacion_incremental;
-  
 
-  odom_pub = node.advertise<nav_msgs::Odometry>("odom", 10);
-  
-  cov_marker_pub = node.advertise<visualization_msgs::Marker>("odom_covariance", 10);
 
-  ros::Subscriber fix_sub = node.subscribe("fix", 10, callback);
+  odom_pub = node.advertise<nav_msgs::Odometry>("odom", 1);
+
+  cov_marker_pub = node.advertise<visualization_msgs::Marker>("odom_covariance", 1);
+
+  ros::Subscriber fix_sub = node.subscribe("fix", 1, callback);
 
   ros::spin();
 }
--- gpsd_client/scripts/gpsd_not_daemon.sh
+++ gpsd_client/scripts/gpsd_not_daemon.sh
@@ -9,4 +9,6 @@ GPS_PORT="/dev/ttyMI4"
 ##echo "%em%em,,nmea/{GGA:0.2,GSA,GST,VTG}:1"  > $GPS_PORT
 #echo "%em%em,,nmea/{GGA,GST}:0.2"  > $GPS_PORT
 
-gpsd -n -N -D 0 $GPS_PORT
+stty speed 115200 <$GPS_PORT
+~/ROS/ull-isaatc/src/gpsd/gpsd -n -N -D 5 $GPS_PORT
+
