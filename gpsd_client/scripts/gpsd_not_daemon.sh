02f73995b7e00fd8cdc2563256e1b2027b161924
verdino <verdino@verdino.(none)>
2013-11-14 17:42:02 +0000

    Add lots of modifications to GPSFix (extended_fix) report to add covariance and others.

--- gpsd_client/scripts/gpsd_not_daemon.sh
+++ gpsd_client/scripts/gpsd_not_daemon.sh
@@ -10,5 +10,6 @@ GPS_PORT="/dev/ttyMI4"
 #echo "%em%em,,nmea/{GGA,GST}:0.2"  > $GPS_PORT
 
 stty speed 115200 <$GPS_PORT
-~/ROS/ull-isaatc/src/gpsd/gpsd -n -N -D 5 $GPS_PORT
-
+~/ROS/ull-isaatc/src/gpsd/gpsd -n -N -D 0 $GPS_PORT
+#/media/VerdinoHD1/usr/local/sbin/gpsd -n -N -D 5 $GPS_PORT
+#/media/VerdinoHD1/home/verdino/launch_Dani/gpsd-3.5/gpsd-3.5/gpsd -n -N -D 5 $GPS_PORT
--- gpsd_client/src/client.cpp
+++ gpsd_client/src/client.cpp
@@ -111,8 +111,17 @@ class GPSDClient {
       GPSFix fix;
       GPSStatus status;
 
-      status.header.stamp = time;
-      fix.header.stamp = time;
+      // adding frame_id so we can visualize it in RViz
+      fix.header.frame_id = "world";
+      /* TODO: Support SBAS and other GBAS. */
+
+      if (use_gps_time) {
+        status.header.stamp = ros::Time(p->fix.time);
+        fix.header.stamp = ros::Time(p->fix.time);
+      } else {
+        status.header.stamp = ros::Time::now();
+        fix.header.stamp = ros::Time::now();
+      }
 
       status.satellites_used = p->satellites_used;
 
@@ -142,65 +151,100 @@ class GPSDClient {
       ROS_DEBUG("Latitude: %f", p->fix.longitude);
       
       ROS_DEBUG("Status: %d", p->status);
-      if ((p->status & STATUS_FIX) && !(check_fix_by_variance && isnan(p->fix.epx))) {
-        ROS_DEBUG("Building fix message");
-        status.status = 0; // FIXME: gpsmm puts its constants in the global
-                           // namespace, so `GPSStatus::STATUS_FIX' is illegal.
 
-        if (p->status & STATUS_DGPS_FIX)
-          status.status |= 18; // same here
+        /* gpsmm pollutes the global namespace with STATUS_,
+       * so we need to use the ROS message's integer values
+       * for status.status
+       */
+      switch (p->status) {
+        case STATUS_NO_FIX:
+          fix.status.status = -1; // NavSatStatus::STATUS_NO_FIX;
+          break;
+        case STATUS_FIX:
+          fix.status.status = 0; // NavSatStatus::STATUS_FIX;
+          break;
+        case STATUS_DGPS_FIX:
+          fix.status.status = 2; // NavSatStatus::STATUS_GBAS_FIX;
+          break;
+      }
 
-        fix.time = p->fix.time;
-        fix.latitude = p->fix.latitude;
-        fix.longitude = p->fix.longitude;
-        fix.altitude = p->fix.altitude;
-        fix.track = p->fix.track;
-        fix.speed = p->fix.speed;
-        fix.climb = p->fix.climb;
+      fix.time = p->fix.time;
+      fix.latitude = p->fix.latitude;
+      fix.longitude = p->fix.longitude;
+      fix.altitude = p->fix.altitude;
+      fix.track = p->fix.track;
+      fix.speed = p->fix.speed;
+      fix.climb = p->fix.climb;
+      
+      #if GPSD_API_MAJOR_VERSION > 3
+      fix.pdop = p->dop.pdop;
+      fix.hdop = p->dop.hdop;
+      fix.vdop = p->dop.vdop;
+      fix.tdop = p->dop.tdop;
+      fix.gdop = p->dop.gdop;
+      #else
+      fix.pdop = p->pdop;
+      fix.hdop = p->hdop;
+      fix.vdop = p->vdop;
+      fix.tdop = p->tdop;
+      fix.gdop = p->gdop;
+      #endif
+      
+      fix.err = p->epe;
+      fix.err_vert = p->fix.epv;
+      fix.err_track = p->fix.epd;
+      fix.err_speed = p->fix.eps;
+      fix.err_climb = p->fix.epc;
+      fix.err_time = p->fix.ept;
+      
+      ROS_DEBUG("FIX_ERR values - epe: %f", p->epe );
+      ROS_DEBUG("FIX_ERR values - epx: %f", p->fix.epx);
+      ROS_DEBUG("FIX_ERR values - epy: %f", p->fix.epy);
+      ROS_DEBUG("FIX_ERR values - epv: %f", p->fix.epv );
+      ROS_DEBUG("FIX_ERR values - track: %f", p->fix.epd );
+      ROS_DEBUG("FIX_ERR values - speed: %f", p->fix.eps );
+      ROS_DEBUG("FIX_ERR values - climb: %f", p->fix.epc );
+      ROS_DEBUG("FIX_ERR values - time: %f", p->fix.ept );
+      
 
-#if GPSD_API_MAJOR_VERSION > 3
-        fix.pdop = p->dop.pdop;
-        fix.hdop = p->dop.hdop;
-        fix.vdop = p->dop.vdop;
-        fix.tdop = p->dop.tdop;
-        fix.gdop = p->dop.gdop;
-#else
-        fix.pdop = p->pdop;
-        fix.hdop = p->hdop;
-        fix.vdop = p->vdop;
-        fix.tdop = p->tdop;
-        fix.gdop = p->gdop;
-#endif
+      ROS_DEBUG("GST utc: %f", p->gst.utctime);
+      ROS_DEBUG("GST rms: %f", p->gst.rms_deviation);
+      ROS_DEBUG("GST maj: %f", p->gst.smajor_deviation);
+      ROS_DEBUG("GST min: %f", p->gst.sminor_deviation);
+      ROS_DEBUG("GST ori: %f", p->gst.smajor_orientation);
+      ROS_DEBUG("GST lat_standard_deviation: %f", p->gst.lat_err_deviation);
+      ROS_DEBUG("GST lon_standard_deviation: %f", p->gst.lon_err_deviation);
+      ROS_DEBUG("GST:alt_standard_deviation: %f", p->gst.alt_err_deviation);
+      
+      /* gpsd reports status=OK even when there is no current fix,
+       * as long as there has been a fix previously. Throw out these
+       * fake results, which have NaN variance
+       */
+       
+      //TODO: añadir aqui la comprobación que se reporta error elíptico, y si es así convertilo a matriz de covarianza (con valores fuera de la diagonal a partir de la orientación de la elipse), que aporta aun más información
+      if (isnan(p->gst.smajor_deviation))
+        ROS_WARN("GPS not reporting oriented error ellipse, falling back to lat-long oriented standard deviation.");  
 
-        fix.err = p->epe;
-        fix.err_vert = p->fix.epv;
-        fix.err_track = p->fix.epd;
-        fix.err_speed = p->fix.eps;
-        fix.err_climb = p->fix.epc;
-        fix.err_time = p->fix.ept;
-        
-        ROS_DEBUG("FIX_ERR values - epe: %f", p->epe );
-        ROS_DEBUG("FIX_ERR values - epx: %f", p->fix.epx);
-        ROS_DEBUG("FIX_ERR values - epy: %f", p->fix.epy);
-        ROS_DEBUG("FIX_ERR values - epv: %f", p->fix.epv );
-        ROS_DEBUG("FIX_ERR values - track: %f", p->fix.epd );
-        ROS_DEBUG("FIX_ERR values - speed: %f", p->fix.eps );
-        ROS_DEBUG("FIX_ERR values - climb: %f", p->fix.epc );
-        ROS_DEBUG("FIX_ERR values - time: %f", p->fix.ept );
-        
-        
-        ROS_DEBUG("GST utc: %f", p->gst.utctime);
-        ROS_DEBUG("GST rms: %f", p->gst.rms_deviation);
-        ROS_DEBUG("GST maj: %f", p->gst.smajor_deviation);
-        ROS_DEBUG("GST min: %f", p->gst.sminor_deviation);
-        ROS_DEBUG("GST ori: %f", p->gst.smajor_orientation);
-        ROS_DEBUG("GST lat_standard_deviation: %f", p->gst.lat_err_deviation);
-        ROS_DEBUG("GST lon_standard_deviation: %f", p->gst.lon_err_deviation);
-        ROS_DEBUG("GST:alt_standard_deviation: %f", p->gst.alt_err_deviation);
+      // PATCH: miramos primero si se reportaron valores de error en desviación estándar en m.
+      
+      if (!isnan(p->gst.lat_err_deviation)) { //los valores al ser covarianza deberían ser ^2
+                                              //, pero no estoy muy seguro y los dejo originales pq se inestabilizan mucho
+                                              
+        fix.position_covariance[0] = p->gst.lat_err_deviation; // * p->gst.lat_err_deviation;
+        fix.position_covariance[4] = p->gst.lon_err_deviation; // * p->gst.lon_err_deviation;
+        fix.position_covariance[8] = p->gst.alt_err_deviation; // * p->gst.alt_err_deviation;
 
-        /* TODO: attitude */
-      } else {
-      	status.status = -1; // STATUS_NO_FIX
+        fix.position_covariance_type = NavSatFix::COVARIANCE_TYPE_DIAGONAL_KNOWN;
+      } else if (!isnan(p->fix.epx)) {
+        ROS_WARN("GPS not reporting lat-long oriented standard deviation, falling back to epx, epy, epv (50-50 confidence).");
+        fix.position_covariance[0] = p->fix.epx;
+        fix.position_covariance[4] = p->fix.epy;
+        fix.position_covariance[8] = p->fix.epv;
+
+        fix.position_covariance_type = NavSatFix::COVARIANCE_TYPE_DIAGONAL_KNOWN;
+      } else if (check_fix_by_variance) {
+        ROS_WARN("GPS not reporting error values and check_fix_by_variance parameter is true, thus we will not publish this fix.");
+        return;
       }
 
       fix.status = status;
